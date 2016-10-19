6efca10bcacf36ec9760e7075aec4ab8d4f548b0
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-19 11:14:34 +0100

    added revisions to docker file

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -5,18 +5,21 @@ docker rm java-app
 find java-app/app/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
 set -e
 
+git clone https://github.com/mo-martin/aws-java-app-build.git ~/scripts
+git clone https://github.com/mo-martin/c1-java-app.git ~/java-app
+
 rm -rf ~/tmp/java-app/
 mkdir -p ~/tmp/java-app/
 
-cp -r ~/java-app/. mkdir -p ~/tmp/java-app/
+cp -r ~/java-app/. ~/tmp/java-app/
 rm -rf ~/java-app/.git
 rm -rf ~/java-app/.gitignore
 
-cp -r ~/tmp/java-app/. ~/synced/docker/java-app/app
+cp -r ~/tmp/java-app/. ~/scripts/docker/java-app/app
 
 
 pushd java-app
-docker build --tag my-node-app:latest .
+docker build --tag java-app:latest .
 popd
 
 docker run --name java-app
